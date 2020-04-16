751ab4f9c998f55e1155bede687be10e7f5e63cf
Dennis Rudolph <Dennis@Denniss-MacBook-Pro.local>
2020-04-16 18:42:03 -0400

    Rock, Paper, Scissors and Eating Cookies solutions

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -8,30 +8,16 @@ import math
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-    ways = 0
-    # Base case
-    if n == 1 or n == 0:
-        return 1
-
-
-
-    return eating_cookies(n-1) + eating_cookies(n-2)
-
-
-​
-def fib(n, cache=None):
     if cache is None:
-        cache = {}
-    # Base case
-    if n <= 2:
-        return 1
-    elif n in cache:
-        return cache[n]
-    else:
-        answer = fib(n-1, cache) + fib(n-2, cache)
-        cache[n] = answer
-        # Recursive call, should move toward base case
-        return answer
+        cache = [0] * (n + 1)
+    if n <= 1:
+        cache[n] = 1
+    elif n == 2:
+        cache[n] = 2
+    elif cache[n] == 0:
+        cache[n] = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
+    return cache[n]
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,33 @@
 
 import sys
 
+
+
 def rock_paper_scissors(n):
-  pass 
+
+    # Base cases
+    if n == 0:
+        return [[]]
+
+    if n == 1:
+        return [["rock"], ["paper"], ["scissors"]]
+
+    answer = []
+
+    for i in rock_paper_scissors(n - 1):
+        temp_rock = i + ["rock"]
+        temp_paper = i + ["paper"]
+        temp_scissors = i + ["scissors"]
+        answer.append(temp_rock)
+        answer.append(temp_paper)
+        answer.append(temp_scissors)
+
+    return answer
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
